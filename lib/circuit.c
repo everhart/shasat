845b8d80e518d4f56e36c56b5ef1c1a464456ace
everhart <rpe@rpe.io>
2017-01-06 07:17:39 -0500

    Wrote the writeOrLogic and writeNotOrLogic functions.

--- lib/circuit.c
+++ lib/circuit.c
@@ -1,17 +1,75 @@
 #include "circuit.h"
 
 int fwriteOrLogic(
-	const char * 	file,
+	FILE * 		stream,
        	size_t		size,
 	uint32_t 	inp1, 
 	uint32_t 	inp2, 
-	uint32_t	res
+	uint32_t	oup
 ) {
+	int res = 0;
+	int clause[3] = { 0, 0, 0 }; //clause buffer
+	
+	for (int i = 0; i < size; i++) {
+		clause[0] = inp1 + i;
+		clause[1] = inp2 + i;
+
+		for (int j = 0; j < 4; j++) {
+			clause[0] = -clause[0];
+			clause[1] = (j % 2 == 0) ? 
+				clause[1] : -clause[1];
+			clause[2] = (clause[0] < 0 || clause[1] < 0) ?
+				oup + i : -(oup + i);
+
+			res = fprintf(
+				stream, "%d %d %d 0\n", 
+				clause[0], clause[1], clause[2]
+			);
+			if (res < 0) {
+				return res;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int fwriteNotOrLogic(
+	FILE * 		stream,
+	size_t		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t	oup	
+) {
+	int res = 0;
+	int clause[3] = { 0, 0, 0 };
+
+	for (int i = 0; i < size; i++) {
+		clause[0] = inp1 + i;
+		clause[1] = inp2 + i;
+
+		for (int j = 0; j < inp2 + i; j++) {
+			clause[0] = -clause[0];
+			clause[1] = (j % 2 == 0) ?
+				clause[1] : -clause[1];
+			clause[2] = (clause[0] > 0 || clause[1] < 0) ?
+				oup + i : (-oup + i);
+
+			res = fprintf(
+				stream, "%d %d %d 0\n", 
+				clause[0], clause[1], clause[2]
+			);
+			if (res < 0) {
+				return res;
+			}	
+		}
+	}
+
 	return 0;
 }
 
 int fwriteAndLogic(
-	const char *	file,
+	FILE *		stream,
 	size_t 		size,
 	uint32_t 	inp1, 
 	uint32_t 	inp2, 
@@ -21,7 +79,7 @@ int fwriteAndLogic(
 }
 
 int fwriteNotAndLogic(
-	const char * 	file,
+	FILE * 		file,
 	size_t 		size,
 	uint32_t 	inp1, 
 	uint32_t 	inp2, 
@@ -31,7 +89,7 @@ int fwriteNotAndLogic(
 }
 
 int fwriteXorLogic(
-	const char *	file,
+	FILE *		file,
 	size_t 		size,
 	uint32_t 	inp1, 
 	uint32_t 	inp2, 
@@ -41,7 +99,7 @@ int fwriteXorLogic(
 }
 
 int fwriteRightShiftLogic(
-	const char *	file,
+	FILE *		file,
 	size_t		size,
 	uint32_t 	inp, 
 	uint32_t 	rsh, 
@@ -51,7 +109,7 @@ int fwriteRightShiftLogic(
 }
 
 int fwriteLeftShiftLogic(
-	const char *	file,
+	FILE *		file,
 	size_t		size,
 	uint32_t 	inp, 
 	uint32_t 	lsh, 
@@ -61,7 +119,7 @@ int fwriteLeftShiftLogic(
 }
 
 int fwriteRightRotateLogic(
-	const char *	file,
+	FILE *		file,
 	size_t		size,
 	uint32_t 	inp, 
 	uint32_t 	rro, 
@@ -71,7 +129,7 @@ int fwriteRightRotateLogic(
 }
 
 int fwriteLeftRotateLogic(
-	const char *	file,
+	FILE *		file,
 	size_t		size,
 	uint32_t 	inp, 
 	uint32_t 	lro, 
@@ -81,7 +139,7 @@ int fwriteLeftRotateLogic(
 }
 
 int fwriteSumLogic(
-	const char *	file,
+	FILE *		file,
 	size_t 		size,
 	uint32_t 	inp1, 
 	uint32_t 	inp2, 
