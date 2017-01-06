b37414890aa2e211fb5a44185cee7fd30e9f3a21
everhart <rpe@rpe.io>
2017-01-06 07:28:55 -0500

    Wrote the writeAndLogic and writeNotAndLogic functions.

--- lib/circuit.c
+++ lib/circuit.c
@@ -73,18 +73,66 @@ int fwriteAndLogic(
 	size_t 		size,
 	uint32_t 	inp1, 
 	uint32_t 	inp2, 
-	uint32_t 	res
+	uint32_t 	oup
 ) {
+	int res = 0;
+	int clause[3] = { 0, 0, 0 };
+
+	for (int i = 0; i < size; i++) {
+		clause[0] = inp1 + i;
+		clause[1] = inp2 + i;
+
+		for (int j = 0; j < inp2 + i; j++) {
+			clause[0] = -clause[0];
+			clause[1] = (j % 2 == 0) ?
+				clause[1] : -clause[1];
+			clause[2] = (clause[0] < 0 && clause[1] < 0) ?
+				oup + i : (-oup + i);
+
+			res = fprintf(
+				stream, "%d %d %d 0\n", 
+				clause[0], clause[1], clause[2]
+			);
+			if (res < 0) {
+				return res;
+			}	
+		}
+	}
+
 	return 0;
 }
 
 int fwriteNotAndLogic(
-	FILE * 		file,
+	FILE * 		stream,
 	size_t 		size,
 	uint32_t 	inp1, 
 	uint32_t 	inp2, 
-	uint32_t 	res
+	uint32_t 	oup
 ) {
+	int res = 0;
+	int clause[3] = { 0, 0, 0 };
+
+	for (int i = 0; i < size; i++) {
+		clause[0] = inp1 + i;
+		clause[1] = inp2 + i;
+
+		for (int j = 0; j < inp2 + i; j++) {
+			clause[0] = -clause[0];
+			clause[1] = (j % 2 == 0) ?
+				clause[1] : -clause[1];
+			clause[2] = (clause[0] > 0 && clause[1] < 0) ?
+				oup + i : (-oup + i);
+
+			res = fprintf(
+				stream, "%d %d %d 0\n", 
+				clause[0], clause[1], clause[2]
+			);
+			if (res < 0) {
+				return res;
+			}	
+		}
+	}
+
 	return 0;
 }
 
