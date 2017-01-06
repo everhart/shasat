a63a9176dd95cd1ecb0d87c740e77e452a6cb554
everhart <rpe@rpe.io>
2017-01-06 18:11:29 -0500

    Added fwriteXorLogic(), fwriteNotXorLogic(), fwriteRightShiftLogic() and fwriteLeftShiftLogic() functions.

--- lib/circuit.c
+++ lib/circuit.c
@@ -8,7 +8,7 @@ int fwriteOrLogic(
 	uint32_t	oup
 ) {
 	int res = 0;
-	int clause[3] = { 0, 0, 0 }; //clause buffer
+	int clause[3] = { 0, 0, 0 };
 	
 	for (int i = 0; i < size; i++) {
 		clause[0] = inp1 + i;
@@ -137,61 +137,162 @@ int fwriteNotAndLogic(
 }
 
 int fwriteXorLogic(
-	FILE *		file,
+	FILE *		stream,
 	size_t 		size,
 	uint32_t 	inp1, 
 	uint32_t 	inp2, 
-	uint32_t 	res
+	uint32_t 	oup
 ) {
+	int res = 0;
+	int clause[3] = { 0, 0, 0 };
+
+	for (int i = 0; i < size; i++) {
+		clause[0] = inp1 + i;
+		clause[1] = inp2 + i;
+
+		for (int j = 0; j < inp2 + i; j++) {
+			clause[0] = -clause[0];
+			clause[1] = (j % 2 == 0) ?
+				clause[1] : -clause[1];
+			clause[2] = (clause[0] < 0 != clause[1] < 0) ?
+				oup + i : -(oup + i);
+
+			res = fprintf(
+				stream, "%d %d %d 0\n", 
+				clause[0], clause[1], clause[2]
+			);
+			if (res < 0) {
+				return res;
+			}	
+		}
+	}
+
+	return 0;
+}
+
+int fwriteNotXorLogic(
+	FILE *		stream,
+	size_t 		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t 	oup
+) {
+	int res = 0;
+	int clause[3] = { 0, 0, 0 };
+
+	for (int i = 0; i < size; i++) {
+		clause[0] = inp1 + i;
+		clause[1] = inp2 + i;
+
+		for (int j = 0; j < inp2 + i; j++) {
+			clause[0] = -clause[0];
+			clause[1] = (j % 2 == 0) ?
+				clause[1] : -clause[1];
+			clause[2] = (clause[0] > 0 != clause[1] < 0) ?
+				oup + i : -(oup + i);
+
+			res = fprintf(
+				stream, "%d %d %d 0\n", 
+				clause[0], clause[1], clause[2]
+			);
+			if (res < 0) {
+				return res;
+			}	
+		}
+	}
+
 	return 0;
 }
 
 int fwriteRightShiftLogic(
-	FILE *		file,
+	FILE *		stream,
 	size_t		size,
 	uint32_t 	inp, 
 	uint32_t 	rsh, 
-	uint32_t 	res
+	uint32_t 	oup
 ) {
+	int res = 0;
+	int clause[2] = { 0, 0 };
+
+	for (int i = size - rsh; i < size; i++) {
+		res = fprintf(stream, "%d 0\n", -(oup + i));
+		if (res < 0) {
+			return res;
+		}
+	}
+
+	for (int i = rsh; i < size; i++) {
+		clause[0] = inp + i;
+		clause[1] = oup + (i - rsh);
+
+		res = fprintf( 
+			stream, "%d %d 0\n %d %d 0\n",
+			-clause[0], clause[1], clause[0], -clause[1]
+		);
+		if (res < 0) {
+			return res;
+		}
+	}
+
 	return 0;
 }
 
 int fwriteLeftShiftLogic(
-	FILE *		file,
+	FILE *		stream,
 	size_t		size,
 	uint32_t 	inp, 
 	uint32_t 	lsh, 
-	uint32_t 	res
+	uint32_t 	oup
 ) {
+	int res = 0;
+	int clause[2] = { 0, 0 };
+
+	for (int i = 0; i < lsh; i++) {
+		res = fprintf(stream, "%d 0\n", -(oup + i));
+	}	
+
+	for (int i = 0; i < size - lsh - 1; i++) {
+		clause[0] = inp + i;
+		clause[1] = oup + (i + lsh);
+
+		res = fprintf(
+			stream, "%d %d 0\n %d %d 0\n",
+			-clause[0], clause[1], clause[0], -clause[1]
+		);
+		if (res < 0) {
+			return res;
+		}
+	}
+
 	return 0;
 }
 
 int fwriteRightRotateLogic(
-	FILE *		file,
+	FILE *		stream,
 	size_t		size,
 	uint32_t 	inp, 
 	uint32_t 	rro, 
-	uint32_t 	res
+	uint32_t 	oup
 ) {
 	return 0;
 }
 
 int fwriteLeftRotateLogic(
-	FILE *		file,
+	FILE *		stream,
 	size_t		size,
 	uint32_t 	inp, 
 	uint32_t 	lro, 
-	uint32_t 	res
+	uint32_t 	oup
 ) {
 	return 0;
 }
 
 int fwriteSumLogic(
-	FILE *		file,
+	FILE *		stream,
 	size_t 		size,
 	uint32_t 	inp1, 
 	uint32_t 	inp2, 
-	uint32_t 	res, 
+	uint32_t 	oup, 
 	uint32_t 	crr
 ) {
 	return 0;
