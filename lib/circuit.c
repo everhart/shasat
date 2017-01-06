da3b1fb416e1d0df79ed7116b7bbf6fcb98339b0
everhart <rpe@rpe.io>
2017-01-06 05:57:20 -0500

    Prototyped circuit reduction functions.

--- lib/circuit.c
+++ lib/circuit.c
@@ -1,1 +1,92 @@
 #include "circuit.h"
+
+int fwriteOrLogic(
+	const char * 	file,
+       	size_t		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t	res
+) {
+	return 0;
+}
+
+int fwriteAndLogic(
+	const char *	file,
+	size_t 		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t 	res
+) {
+	return 0;
+}
+
+int fwriteNotAndLogic(
+	const char * 	file,
+	size_t 		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t 	res
+) {
+	return 0;
+}
+
+int fwriteXorLogic(
+	const char *	file,
+	size_t 		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t 	res
+) {
+	return 0;
+}
+
+int fwriteRightShiftLogic(
+	const char *	file,
+	size_t		size,
+	uint32_t 	inp, 
+	uint32_t 	rsh, 
+	uint32_t 	res
+) {
+	return 0;
+}
+
+int fwriteLeftShiftLogic(
+	const char *	file,
+	size_t		size,
+	uint32_t 	inp, 
+	uint32_t 	lsh, 
+	uint32_t 	res
+) {
+	return 0;
+}
+
+int fwriteRightRotateLogic(
+	const char *	file,
+	size_t		size,
+	uint32_t 	inp, 
+	uint32_t 	rro, 
+	uint32_t 	res
+) {
+	return 0;
+}
+
+int fwriteLeftRotateLogic(
+	const char *	file,
+	size_t		size,
+	uint32_t 	inp, 
+	uint32_t 	lro, 
+	uint32_t 	res
+) {
+	return 0;
+}
+
+int fwriteSumLogic(
+	const char *	file,
+	size_t 		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t 	res, 
+	uint32_t 	crr
+) {
+	return 0;
+}
--- lib/circuit.h
+++ lib/circuit.h
@@ -1,4 +1,72 @@
 #ifndef CIRCUIT_H
 #define CIRCUIT_H
 
+#include <stdio.h>
+#include <stdint.h>
+
+int fwriteOrLogic(
+	const char * 	file,
+       	size_t		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t	res
+);
+int fwriteAndLogic(
+	const char *	file,
+	size_t 		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t 	res
+);
+int fwriteNotAndLogic(
+	const char * 	file,
+	size_t 		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t 	res
+);
+int fwriteXorLogic(
+	const char *	file,
+	size_t 		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t 	res
+);
+int fwriteRightShiftLogic(
+	const char *	file,
+	size_t		size,
+	uint32_t 	inp, 
+	uint32_t 	rsh, 
+	uint32_t 	res
+);
+int fwriteLeftShiftLogic(
+	const char *	file,
+	size_t		size,
+	uint32_t 	inp, 
+	uint32_t 	lsh, 
+	uint32_t 	res
+);
+int fwriteRightRotateLogic(
+	const char *	file,
+	size_t		size,
+	uint32_t 	inp, 
+	uint32_t 	rro, 
+	uint32_t 	res
+);
+int fwriteLeftRotateLogic(
+	const char *	file,
+	size_t		size,
+	uint32_t 	inp, 
+	uint32_t 	lro, 
+	uint32_t 	res
+);
+int fwriteSumLogic(
+	const char *	file,
+	size_t 		size,
+	uint32_t 	inp1, 
+	uint32_t 	inp2, 
+	uint32_t 	res, 
+	uint32_t 	crr
+);
+
 #endif
