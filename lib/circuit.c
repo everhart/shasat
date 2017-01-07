252fcff772d0292dad645cca5510e0e137656eec
everhart <rpe@rpe.io>
2017-01-07 10:39:46 -0500

    Added some parentheses in fwriteAssignLogic() for cleanliness.

--- lib/circuit.c
+++ lib/circuit.c
@@ -16,7 +16,7 @@ int fwriteAssignLogic(
 		for (int i = 0; i < 2; i++) {
 			res = fprintf(
 				stream, "%d %d 0\n",
-				clause[0] *= -1, clause[1] *= -1
+				(clause[0] *= -1), (clause[1] *= -1)
 			);
 			if (res < 0) {
 				return res;
