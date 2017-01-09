292440685b4379bcca7ff5649354bde3efb44e69
everhart <rpe@rpe.io>
2017-01-09 09:58:46 -0500

    Changed function prototypes to fit the revised paradigm.

--- sha1sat/sha1sat.h
+++ sha1sat/sha1sat.h
@@ -7,10 +7,6 @@
 
 #define SHA1_ATOMS_PER_CHUNK 35136
 
-uint32_t indexRoundConstBitSHA1(
-	uint32_t	word,
-	uint32_t	bit
-);
 uint32_t indexHashBitSHA1(
 	uint32_t 	chunk,
 	uint32_t	word,
@@ -28,6 +24,22 @@ uint32_t indexWorkingVarBitSHA1(
 );
 uint32_t indexBufferBitSHA1(
 	uint32_t	chunk,
+	uint32_t	word,
+	uint32_t	bit
+);
+uint32_t indexRoundConstBitSHA1(
+	size_t 		msize,
+	uint32_t	word,
+	uint32_t	bit
+);
+uint32_t indexDigestBitSHA1(
+	size_t 		msize,
+	uint32_t	word,
+	uint32_t	bit
+);
+uint32_t indexMessageBitSHA1(
+	size_t		msize,
+	size_t		wsize,
 	uint32_t	word,
 	uint32_t	bit
 );
