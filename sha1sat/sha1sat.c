e858b118e7c9d5912bc3c4887cf0b0d39587d7f8
everhart <rpe@rpe.io>
2017-01-09 10:09:08 -0500

    Corrected all index functions.

--- sha1sat/sha1sat.c
+++ sha1sat/sha1sat.c
@@ -1,21 +1,14 @@
 #include "sha1sat.h"
 
-uint32_t indexRoundConstBitSHA1(
-	uint32_t	word,
-	uint32_t	bit
-) {
-	return (word * 32) + bit;
-}
-
 uint32_t indexHashBitSHA1(
 	uint32_t 	chunk,
 	uint32_t	word,
 	uint32_t 	bit
 ) {
 	return (chunk * SHA1_ATOMS_PER_CHUNK) + 
-	       129 + 
 	       (word * 32) +
-	       bit;
+	       bit +
+	       1;	//160 hash bits
 }
 
 uint32_t indexMessageScheduleBitSHA1(
@@ -24,9 +17,9 @@ uint32_t indexMessageScheduleBitSHA1(
 	uint32_t	bit
 ) {
 	return (chunk * SHA1_ATOMS_PER_CHUNK) +
-	       290 +
 	       (word * 32) +
-	       bit;
+	       bit +
+	       161;	//2560 message schedule bits
 }
 
 uint32_t indexWorkingVarBitSHA1(
@@ -35,9 +28,9 @@ uint32_t indexWorkingVarBitSHA1(
 	uint32_t	bit
 ) {
 	return (chunk * SHA1_ATOMS_PER_CHUNK) +
-	       2851 +
 	       (word * 32) +
-	       bit;
+	       bit +
+	       2721;	//15520 working variable bits
 }
 
 uint32_t indexBufferBitSHA1(
@@ -46,7 +39,7 @@ uint32_t indexBufferBitSHA1(
 	uint32_t	bit
 ) {
 	return (chunk * SHA1_ATOMS_PER_CHUNK) +
-	       3236 +
+	       18241 +
 	       (word * 32) +
-	       bit;
+	       bit;	//16896 buffer bits
 }
