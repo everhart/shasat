71666beb6cdf5dd50e0ff885351c26a535228e81
everhart <rpe@rpe.io>
2017-01-09 07:04:11 -0500

    Named functions for indexing results of SHA-1 arithmetic and bitwise operations.

--- sha1sat/sha1sat.h
+++ sha1sat/sha1sat.h
@@ -5,4 +5,11 @@
 #include <stdint.h>
 #include "../lib/circuit.h"
 
+uint32_t indexAndBitSHA1();
+uint32_t indexNotAndBitSHA1();
+uint32_t indexOrBitSHA1();
+uint32_t indexXorBitSHA1();
+uint32_t indexLeftRotateBitSHA1();
+uint32_t indexSumBitSHA1();
+
 #endif
