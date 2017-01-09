62fab90783f03c4eac36624ceeed57cf394dbb12
everhart <rpe@rpe.io>
2017-01-09 08:30:21 -0500

    Prototyped functions.

--- sha1sat/sha1sat.h
+++ sha1sat/sha1sat.h
@@ -5,11 +5,40 @@
 #include <stdint.h>
 #include "../lib/circuit.h"
 
-uint32_t indexMessageBitSHA1();
-uint32_t indexHashBitSHA1();
-uint32_t indexRoundConstBitSHA1();
-uint32_t indexMessageScheduleBitSHA1();
-uint32_t indexWorkingVarBitSHA1();
-uint32_t indexBufferBitSHA1();
+uint32_t indexMessageBitSHA1(
+	uint32_t 	word,
+	uint32_t 	bit
+);
+uint32_t indexRoundConstBitSHA1(
+	uint32_t	word,
+	uint32_t	bit
+);
+uint32_t indexHashBitSHA1(
+	uint32_t 	chunk,
+	uint32_t	word,
+	uint32_t 	bit
+);
+uint32_t indexMessageScheduleBitSHA1(
+	uint32_t	chunk,
+	uint32_t	word,
+	uint32_t	bit
+);
+uint32_t indexWorkingVarBitSHA1(
+	uint32_t 	chunk,
+	uint32_t 	word,
+	uint32_t	bit
+);
+
+uint32_t indexBufferBitSHA1(
+	uint32_t	chunk,
+	uint32_t	word,
+	uint32_t	bit
+);
+
+uint32_t indexDigestBitSHA1(
+	uint32_t 	chunk,
+	uint32_t 	word,
+	uint32_t 	bit
+);
 
 #endif
