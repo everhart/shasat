6cfa02c9d87628dbeb3b04309ab6f4342bfd2bfe
everhart <rpe@rpe.io>
2017-01-10 09:00:36 -0500

    Added code for preprocessSHA1() function.

--- sha1sat/sha1sat.c
+++ sha1sat/sha1sat.c
@@ -44,10 +44,56 @@ uint32_t indexBufferBitSHA1(
 	       bit;	//16896 buffer bits
 }
 
+int preprocessSHA1(
+	FILE *		stream,
+	size_t		msize
+) {
+	int res = 0;
+	uint64_t osize = msize;
+
+	//append the bit '1' to the message
+	msize++;
+	res = fwriteAtom(
+		stream, indexMessageBitSHA1(osize, msize)
+	);
+	if (res < 0) {
+		return res;
+	}
+
+	//append '0' to the message until message length % 512 is 448
+	while (msize % 512 != 448) {
+		msize++;
+		res = fwriteAtom(
+			stream, -indexMessageBitSHA1(osize, msize)
+		);
+		if (res < 0) {
+			return res;
+		}
+	}
+
+	//append original message length to the message as a 64 bit 
+	//big-endian integer
+	for (int i = 63; i >= 0; i--) {
+		msize++;
+		res = fwriteAtom(
+			stream, 
+			indexMessageBitSHA1(
+				osize, 
+				((osize >> i) & 1) == 1 ? msize : -msize
+			)
+		);
+		if (res < 0) {		
+			return res;
+		}
+	}
+
+	return msize;
+}
+
 int sha1sat(
 	const char * 	dimacs, 
 	size_t 		msize, 
 	const char * 	digest
-) {
+) {	
 	return 0;	
 }
