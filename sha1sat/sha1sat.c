701f4f55f731200d1c80f793fb53da4884cb7f80
everhart <rpe@rpe.io>
2017-01-20 09:43:39 -0500

    Removed preprocessSHA1() function for now.

--- sha1sat/sha1sat.c
+++ sha1sat/sha1sat.c
@@ -2,7 +2,7 @@
 
 int w[80]; 
 
-uint32_t indexHashBitSHA1(
+uint32_t indexHshBitSHA1(
 	uint32_t 	chunk,
 	uint32_t	word,
 	uint32_t 	bit
@@ -10,7 +10,7 @@ uint32_t indexHashBitSHA1(
 	return 0;
 }
 
-uint32_t indexMessageScheduleBitSHA1(
+uint32_t indexMsaBitSHA1(
 	uint32_t	chunk,
 	uint32_t	word,
 	uint32_t	bit
@@ -18,7 +18,7 @@ uint32_t indexMessageScheduleBitSHA1(
 	return 0;
 }
 
-uint32_t indexWorkingVarBitSHA1(
+uint32_t indexWvrBitSHA1(
 	uint32_t 	chunk,
 	uint32_t 	word,
 	uint32_t	bit
@@ -26,7 +26,7 @@ uint32_t indexWorkingVarBitSHA1(
 	return 0;
 }
 
-uint32_t indexGenericBitSHA1(
+uint32_t indexGnrBitSHA1(
 	uint32_t	chunk,
 	uint32_t	word,
 	uint32_t	bit
@@ -34,15 +34,16 @@ uint32_t indexGenericBitSHA1(
 	return 0;
 }
 
-uint32_t indexRoundConstBitSHA1(
+uint32_t indexRndBitSHA1(
 	size_t 		msize,
 	uint32_t	bit
 ) {
 	return 0;
 }
 
-uint32_t indexMessageBitSHA1(
-	size_t		msize,
+uint32_t indexMsgBitSHA1(
+	uint32_t	chunk,
+	uint32_t 	word,
 	uint32_t	bit
 ) {
 	return 0;
@@ -378,50 +379,6 @@ int fwriteHshClausesSHA1(
 	return 0;
 }
 
-int preprocessSHA1(
-	FILE *		stream,
-	size_t		msize
-) {
-	int res = 0;
-	uint64_t osize = msize;
-
-	//append the bit '1' to the message
-	msize++;
-	res = fwriteAtom(
-		stream, indexMessageBitSHA1(osize, msize)
-	);
-	if (res < 0) {
-		return res;
-	}
-
-	//append '0' to the message until message length % 512 is 448
-	while (msize % 512 != 448) {
-		msize++;
-		res = fwriteAtom(
-			stream, -indexMessageBitSHA1(osize, msize)
-		);
-		if (res < 0) {
-			return res;
-		}
-	}
-
-	//append original message length to the message as a 64 bit 
-	//big-endian integer
-	for (int i = 63; i >= 0; i--) {
-		msize++;
-		res = fwriteAtom(
-			stream,
-			((osize >> i & 1) == 1 ? 1 : -1) *
-				indexMessageBitSHA1(osize, msize)
-		);
-		if (res < 0) {		
-			return res;
-		}
-	}
-
-	return msize;
-}
-
 int sha1sat(
 	FILE * 		stream, 
 	size_t 		msize, 
@@ -432,6 +389,16 @@ int sha1sat(
 
 	//for each chunk
 	for (int i = 0; i < chcount; i++) {
+		for (int j = 0; j < 16; j++) {
+			res = fwriteMsgClausesSHA1(
+				stream,
+				indexMsgBitSHA1(i, j, 0),
+				indexMsaBitSHA1(i, j, 0)
+			);
+			if (res < 0) {
+				return -1;
+			}
+		}
 	}
 
 	return 0;	
