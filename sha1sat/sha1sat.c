336d31efe5dc3ab724d9626612280703bc48b29d
everhart <rpe@rpe.io>
2017-01-23 11:21:14 -0500

    Removed preprocessSHA1() function in favor of preprocessSHA() function in '../shasat.c'.

--- sha1sat/sha1sat.c
+++ sha1sat/sha1sat.c
@@ -469,49 +469,6 @@ int fwriteRoundConstantAtomsSHA1(
 	return oup;
 }
 
-int preprocessSHA1(
-	FILE *		stream,
-	size_t		msize
-) {
-	int res = 0,
-	    atom = 0;
-	const uint64_t osize = msize;
-
-	//append the bit '1' to the message
-	msize++;
-	res = fprintf(
-		stream, "%zd 0\n", msize
-	);
-	if (res < 0) {
-		return -1;
-	}
-
-	//append '0' to the message until message length % 512 is 448
-	while (msize % 512 != 448) {
-		msize++;
-		res = fprintf(
-			stream, "%zd 0\n", -msize
-		);
-		if (res < 0) {
-			return -1;
-		}
-	}
-
-	//append original message length to the message as a 64 bit
-	//big-endian integer
-	for (int i = 63; i >= 0; i--) {
-		atom = msize * 
-		       ((osize >> i & 1) ? 1 : -1);
-
-		res = fprintf(stream, "%d 0\n", atom);
-		if (res < 0) {
-			return -1;
-		}
-	}
-
-	return msize;
-}
-
 int sha1sat(
 	FILE * 		stream, 
 	size_t 		msize, 
