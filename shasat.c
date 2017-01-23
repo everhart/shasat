6652547b8917fe53aafdfe046685c94f1048b68f
everhart <rpe@rpe.io>
2017-01-23 10:14:53 -0500

    Added fwriteAtoms64() function.

--- shasat.c
+++ shasat.c
@@ -25,3 +25,29 @@ int fwriteAtoms32(
 
 	return 0;
 }
+
+int fwriteAtoms64(
+	FILE *		stream,
+	const uint64_t	inp[],
+	const uint64_t	oup[],
+	uint64_t	leng
+) {
+	int res = 0,
+	    atom = 0;
+
+	for (int i = 0; i < leng; i++) {
+		for (int j = 0; j < sizeof(uint64_t); j++) {
+			atom = oup[i] * 
+			       (inp[j] >> i & 1) ? 1 : -1;
+
+			res = fprintf(
+				stream, "%d 0\n", atom
+			);
+			if (res < 0) {
+				return -1;
+			}
+		}
+	}
+
+	return 0;
+}
