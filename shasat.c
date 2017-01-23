dc4217aa033ca98523f5e580016a8a47478f14ff
everhart <rpe@rpe.io>
2017-01-23 10:13:26 -0500

    Changed name of fwriteAtoms() function to fwriteAtoms32().

--- shasat.c
+++ shasat.c
@@ -1,15 +1,15 @@
 #include "shasat.h"
 
-int fwriteAtoms(
+int fwriteAtoms32(
 	FILE *		stream,
 	const uint32_t	inp[],
 	const uint32_t	oup[],
-	size_t		size
+	uint64_t	leng
 ) {
 	int res = 0,
 	    atom = 0;
 
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < leng; i++) {
 		for (int j = 0; j < sizeof(uint32_t); j++) {
 			atom = oup[i] * 
 			       (inp[j] >> i & 1) ? 1 : -1;
