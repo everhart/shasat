f704999846acdbce1398c06e43cad0e1cdc42e61
everhart <rpe@rpe.io>
2017-01-23 09:51:06 -0500

    Added fwriteAtoms() function prototype.

--- shasat.h
+++ shasat.h
@@ -1,6 +1,15 @@
 #ifndef SHASAT_H
 #define SHASAT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
+int fwriteAtoms(
+	FILE *		stream,
+	const uint32_t	inp[],
+	const uint32_t 	oup[],
+	size_t		size
+);
 
 #endif
