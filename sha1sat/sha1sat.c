13ad45e81369043fe348ebc1379d29f9c0b68c79
everhart <rpe@rpe.io>
2017-01-24 10:58:34 -0500

    Added prototype for fwriteChunkClausesSHA1() function.

--- sha1sat/sha1sat.c
+++ sha1sat/sha1sat.c
@@ -2,16 +2,17 @@
 
 int fwriteChunkClausesSHA1(
 	FILE *		stream,
-	const uint32_t	inp,
-	uint32_t	oup
+	uint32_t	word,
+	uint32_t	msa[80],
+	uint32_t	idx
 ) {
 	int res = 0,
 	    ante = 0,
 	    cons = 0;
 
 	for (int i = 0; i < 32; i++) {
-		ante = inp + i;
-		cons = oup + i;
+		ante = word + i;
+		cons = msa[idx] + i;
 
 		res = fprintf(
 			stream, "%d %d 0\n %d %d 0\n",
@@ -335,5 +336,6 @@ int sha1sat(
 	size_t 		msize, 
 	const char * 	digest
 ) {
+	
 	return 0;
 }
