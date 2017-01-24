fb3697a809b54164ff50d1fcce6e6062678379a2
everhart <rpe@rpe.io>
2017-01-24 09:10:19 -0500

    Added HHSHA1 and WVSHA1 structures.

--- sha1sat/sha1sat.h
+++ sha1sat/sha1sat.h
@@ -3,9 +3,27 @@
 
 #include <stdio.h>
 #include <stdint.h>
-#include "../lib/circuit.h"
+#include <stdbool.h>
+#include <string.h>
+#include "../shasat.h"
 
 #define SHA1_ATOMS_PER_CHUNK 35136
+
+typedef struct HHSHA1 {
+	uint32_t h0,
+		 h1,
+		 h2,
+		 h3,
+		 h4;
+} HHSHA1;
+
+typedef struct WVSHA1 {
+	uint32_t a,
+		 b,
+		 c,
+		 d,
+		 e;
+} WVSHA1;
 
 int sha1sat(
 	FILE *		stream, 
