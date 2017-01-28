8816e35547fffb445c55090dee9cda81d4b984f0
everhart <rpe@rpe.io>
2017-01-28 09:09:58 -0500

    Re-added WvSha1 and HhSha1 structures.

--- sha1sat/sha1sat.c
+++ sha1sat/sha1sat.c
@@ -152,18 +152,15 @@ int fwriteFClausesSHA1(
 
 int fwriteTempClausesSHA1Re(
 	FILE *		stream,
-	WVSHA1		wv,
-	uint32_t	f,
-	uint32_t	k,
-	uint32_t	w,
-	uint32_t	temp,
-	uint32_t	inc
+	uint32_t 	inp[5],
+	uint32_t	oup[8]
 ) {
-	int res = 0,
-	    ante = { 0 },
-	    cons = { 0 };
+	int res = 0;
 
-	uint32_t p = 0, q = 0;
+	uint32_t ante[3] = { 0 },
+		 cons[2] = { 0 },
+		 p = 0, 
+		 q = 0;
 
 	bool tmp = 0,
 	     crr = 0,
@@ -181,6 +178,36 @@ int fwriteTempClausesSHA1Re(
 
 		//for each '+' operator in the expression
 		//temp = f + e + k + w[i] + (a lro 5)
+		for (int j = 0; j < 5; j++) {
+			//append clauses representing the outer add
+			//operation
+			if (i < (1 << 2)) {
+				ante[0] = perm[1] ? p : -p;
+				ante[1] = perm[2] ? q : -q;	
+
+				cons[0] = oup[j * 2] *
+					((perm[1] ^ perm[2]) ? 1 : -1);
+				cons[1] = oup[j * 2 + 1] *  
+					((perm[1] && perm[2]) ? 1 : -1);
+
+				res = fprintf(
+					stream, 
+					"%1$d %2$d %3$d 0\n"
+					"%1$d %2$d %4$d 0\n",
+					-ante[0], -ante[1], 
+					cons[0], cons[1]
+				);
+				if (res < 0) {
+					return -1;
+				}
+			}
+
+			//append clauses representing all inner add
+			//operations
+			for (int k = 1; k < 32; k++) {
+
+			}
+		}
 	}
 
 	return 0;
@@ -191,12 +218,13 @@ int fwriteWorkingVarClausesSHA1(
 	const uint32_t	inp[],
 	const uint32_t	oup[]
 ) {
-	int res = 0,
-	    ante = 0,
-	    cons = 0;
+	int res = 0;
 
 	for (int j = 0; j < 5; j++) {
 		for (int k = 0; k < 32; k++) { 
+			uint32_t ante = inp[j],
+				 cons = 0;
+
 			if (j == 2) {
 				cons = (k >= 2) ? 
 					oup[j] + (k - 2) : oup[j] + k;
@@ -221,15 +249,14 @@ int fwriteWorkingVarClausesSHA1(
 
 int fwriteHashClausesSHA1(
 	FILE *		stream,
-	const uint32_t	inp[],
-	const uint32_t	oup[]
+	uint32_t	inp[10],
+	uint32_t	oup[10]
 ) {
 	int res = 0,
 	    ante[3] = { 0 },
 	    cons[2] = { 0 };
 
-	uint32_t p = 0,
-		 q = 0;
+	uint32_t buf[4] = { 0 };
 
 	bool tmp = 0,
 	     crr = 0,
@@ -243,18 +270,21 @@ int fwriteHashClausesSHA1(
 			perm[j] = tmp & 1;
 		}
 
-		for (int j = 0; j < 5; j++) {
-			p = inp[2 * j];
-			q = inp[2 * j + 1];
+		for (int j = 0; j < 5; j++) {			
+			buf[0] = inp[2 * j];
+			buf[1] = inp[2 * j + 1];
+			buf[2] = oup[2 * j];
+			buf[3] = oup[2 * j + 1];
 
 			if (i < (1 << 2)) {
-				ante[0] = p * (perm[1] ? 1 : -1);
-				ante[1] = q * (perm[2] ? 1 : -1);
+				ante[0] = perm[1] ? buf[0] : -buf[0];
+				ante[1] = perm[2] ? buf[1] : -buf[1];
 
 				cons[0] = (perm[1] ^ perm[2]) ? 
-					oup[j * 2] : -oup[j * 2];
+					buf[2] : -buf[2];
 				cons[1] = (perm[1] && perm[2]) ?
-					oup[j * 2 + 1] : -oup[j * 2 + 1];
+					buf[3] : -buf[3];
+
 
 				res = fprintf(
 					stream, 
@@ -269,15 +299,16 @@ int fwriteHashClausesSHA1(
 			}
 
 			for (int k = 1; k < 32; k++) {
-				ante[0] = (p + k) * (perm[0] ? 1 : -1);
-				ante[1] = (q + k) * (perm[1] ? 1 : -1);
+				ante[0] = perm[0] ? 
+					(buf[0] + k) : -(buf[0] + k);
+				ante[1] = perm[1] ? 
+					(buf[1] + k) : -(buf[1] + k);
 				ante[2] = oup[(j - 1) * 2 + 1] + k;
 
 				cons[0] = (perm[0] ^ perm[1] ^ perm[2]) ?
 					oup[j * 2] : -oup[j * 2];
-				cons[1] = (
-					(perm[0] | perm[1]) & perm[2]
-				) ? oup[j * 2 + 1] : -oup[j * 2 + 1];
+				cons[1] = (perm[0] | perm[1]) & perm[2] ?
+					oup[j * 2 + 1] : -oup[j * 2 + 1];
 
 				res = fprintf(
 					stream, 
