867d58c419554a6c16619adac1cb5f93d7bb6138
everhart <rpe@rpe.io>
2017-01-27 22:15:00 -0500

    Added prototypes for index bit (atom enumeration) functions.

--- sha2sat/sha256sat.h
+++ sha2sat/sha256sat.h
@@ -7,6 +7,43 @@
 
 #include "../shasat.h"
 
+#define ATOMS_PER_CHUNK_SHA256 0
 
+uint32_t indexKBitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexWBitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexWvBitSha256(
+	uint32_t type, uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexHhBitSha256(
+	uint32_t type, uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexEp0BitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexEp1BitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexSig0BitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexSig1BitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexChBitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexMajBitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexTemp1BitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
+uint32_t indexTemp2BitSha256(
+	uint32_t chunk, uint32_t word, uint32_t bit
+);
 
 #endif
