13eb92da338a4cb34f538e1a0f9a35051e4f6c79
everhart <rpe@rpe.io>
2017-01-29 10:38:08 -0500

    Added prototypes for all bitops functions.

--- lib/bitops.h
+++ lib/bitops.h
@@ -4,7 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
+int bitValue32(uint32_t word, uint32_t pos);
+int bitValue64(uint64_t word, uint32_t pos);
 
+uint32_t bitPositionRsh(uint32_t wsize, uint32_t pos, uint32_t rsh);
+uint32_t bitPositionRro(uint32_t wsize, uint32_t pos, uint32_t rro);
+uint32_t bitPositionLsh(uint32_t wsize, uint32_t pos, uint32_t lsh);
+uint32_t bitPositionLro(uint32_t wsize, uint32_t pos, uint32_t Lro);
 
 #endif
