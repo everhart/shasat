a9d1928110c5bf745aa02e5f00163caba93bf93b
everhart <rpe@rpe.io>
2017-01-29 11:37:18 -0500

    Added code for bitValue64() function.

--- lib/bitops.c
+++ lib/bitops.c
@@ -8,3 +8,10 @@ int bitValue32(uint32_t word, uint32_t pos) {
 	return (word >> pos) & 1;
 }
 
+int bitValue64(uint64_t word, uint32_t pos) {
+	if (pos >= 64) {
+		return -1;
+	}
+
+	return (word >> pos) & 1;
+}
