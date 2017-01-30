1d7adfc527f831241a394016d2a0fc01b1d0cca6
everhart <rpe@rpe.io>
2017-01-30 11:16:27 -0500

    Removed atomops.c/h, as the relevant functions are now present in routines.c/h.

--- lib/atomops.c
+++
@@ -1,5 +0,0 @@
-#include "./atomops.h"
-
-inline int signAtom(int atom, int state) {
-	return (state) ? atom : -atom;
-}
--- lib/atomops.h
+++
@@ -1,8 +0,0 @@
-#ifndef ATOMOPS_H
-#define ATOMOPS_H
-
-#include <stdio.h>
-
-inline int signAtom(int atom, int state);
-
-#endif
