b90f20ef58504779ad349ed0da775808af2d3d48
everhart <rpe@rpe.io>
2017-01-30 11:14:32 -0500

    Changed paramater and function return types in various functions.

--- lib/routines.h
+++ lib/routines.h
@@ -3,12 +3,13 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 int bitValue32(uint32_t word, uint32_t pos);
 int bitValue64(uint64_t word, uint32_t pos);
 int bitPositionRro(size_t wsize, uint32_t pos, uint32_t rro);
 int bitPositionLro(size_t wsize, uint32_t pos, uint32_t lro);
-inline int signAtom(int atom, int state);
-inline int * nextPermutation(int *arr, size_t leng);
+inline int signAtom(int atom, bool state);
+bool * nextPermutation(bool *arr, size_t leng);
 
 #endif
