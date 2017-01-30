80e6839b22ef5b6c6e0034738d7109a0ed29d795
everhart <rpe@rpe.io>
2017-01-30 10:56:12 -0500

    Added routines.h header, containing methods from bitops.c/h and atomops.c/h.

---
+++ lib/routines.h
@@ -0,0 +1,14 @@
+#ifndef ROUTINES_C
+#define ROUTINES_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+int bitValue32(uint32_t word, uint32_t pos);
+int bitValue64(uint64_t word, uint32_t pos);
+int bitPositionRro(size_t wsize, uint32_t pos, uint32_t rro);
+int bitPositionLro(size_t wsize, uint32_t pos, uint32_t lro);
+inline int signAtom(int atom, int state);
+inline int * nextPermutation(int *arr, size_t leng);
+
+#endif
