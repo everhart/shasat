fb7bf9d598b7439e1e4010c0150248f8359d175e
everhart <rpe@rpe.io>
2017-01-30 10:40:04 -0500

    Removed all prototypes, structures and define macros, except for sha1sat().

--- sha1sat/sha1sat.h
+++ sha1sat/sha1sat.h
@@ -9,56 +9,6 @@
 
 #define SHA1_ATOMS_PER_CHUNK 35136
 
-typedef struct WvSha1 {
-	uint32_t a,
-		 b,
-		 c, 
-		 d,
-		 e;
-} WvSha1;
-
-typedef struct HhSha1 {
-	uint32_t h0,
-		 h1,
-		 h2, 
-		 h3,
-		 h4;
-} HhSha1;
-
-int fwriteMsClausesSha1(
-	FILE *		stream,
-	uint32_t	ms[80],
-	uint32_t	idx
-);
-
-int fwriteFClausesSha1(
-	FILE *		stream,
-	WvSha1		wv,
-	uint32_t	idx
-);
-
-int fwriteTempClausesSha1(
-	FILE *		stream,
-	WvSha1		wv,
-	uint32_t 	f,
-	uint32_t 	k,
-	uint32_t	ms,
-	uint32_t 	temp,
-	uint32_t	inc
-);
-
-int fwriteWvlausesSHA1(
-	FILE *		stream,
-	WvSha1 *	wv
-);
-
-int fwriteHhClausesSha1(
-	FILE *		stream,
-	HhSha1 *	hh,
-	WvSha1		wv,
-	uint32_t 	inc
-);
-
 int sha1sat(
 	FILE *		stream, 
 	size_t 		msize, 
