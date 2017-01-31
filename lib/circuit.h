b061d1e2d26db5e5d6d0a5058664bca7d21c6819
everhart <rpe@rpe.io>
2017-01-31 10:27:14 -0500

    Added prototypes for various fwrite functions.

--- lib/circuit.h
+++ lib/circuit.h
@@ -9,11 +9,46 @@ int fwriteAtoms32(
 	const uint32_t 	oup[],
 	uint64_t	leng
 );
+
 int fwriteAtoms64(
 	FILE *		stream,
 	const uint32_t	inp[],
 	const uint64_t 	oup[],
 	uint64_t	leng
+);
+
+int fwriteAssignClauses(
+	FILE *		stream,
+	size_t		wsize,
+	uint32_t	inp,
+	uint32_t	oup	
+);
+
+int fwriteRshClauses(
+	FILE *		stream,
+	size_t		wsize,
+	uint32_t	inp,
+	uint32_t	oup,
+	uint32_t	rsh
+);
+
+int fwriteSumClauses(
+	FILE *		stream,
+	size_t		wsize,
+	uint32_t	inp1,
+	uint32_t	inp2,
+	uint32_t	sum,
+	uint32_t	crr
+);
+
+int fwriteLroSumClauses(
+	FILE *		stream,
+	size_t		wsize,
+	uint32_t	inp1,
+	uint32_t	inp2,
+	uint32_t	sum,
+	uint32_t	crr,
+	uint32_t	lro
 );
 
 #endif
