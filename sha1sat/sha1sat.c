e1f084339cf6824a797f856fbeff7b6f58545d0b
everhart <rpe@rpe.io>
2017-02-01 12:37:13 -0500

    Added comments denoting how many of each index type there are.

--- sha1sat/sha1sat.c
+++ sha1sat/sha1sat.c
@@ -58,92 +58,92 @@ static index_t indexK(uint32_t ccount, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * ccount + 	//amount of previous indices
 	       idx * 32 + 			//word index
 	       bit;				//bit index
-}
+}	//128 k indices
 
 static index_t indexMessage(uint32_t ccount, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * ccount + 128 +  
 	       idx * 32 +
 	       bit;	
-}
+}	//arbitrary amount of message indicies
 
 static index_t indexW(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 1 +
 	       idx * 32 +
 	       bit;
-}
+}	//2560 w indices
 
 static index_t indexA(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 2561 +
 	       idx * 32 +
 	       bit;
-}
+}	//2560 a indices
 
 static index_t indexB(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 5122 +
 	       idx * 32 +
 	       bit;
-}
+}	//2560 b indices
 
 static index_t indexC(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 7683 +
 	       idx * 32 +
 	       bit;
-}
+}	//2560 c indices
 
 static index_t indexD(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 10244 +
 	       idx * 32 +
 	       bit;
-}
+}	//2560 d indices
 
 static index_t indexE(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 12805 +
 	       idx * 32 +
 	       bit;
-}
+}	//2560 e indices
 
 static index_t indexF(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 15366 +
 	       idx * 32 +
 	       bit;
-}
+}	//2560 f indices
 
 static index_t indexTemp(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 17927 +
 	       idx * 32 +
 	       bit;
-}
+}	//2560 temp indices
 
 static index_t indexH0(uint32_t chunk, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 20488 +
 	       bit;
-}
+}	//32 h0 indices
 
 static index_t indexH1(uint32_t chunk, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 20521 +
 	       bit;
-}
+}	//32 h1 indices
 
 static index_t indexH2(uint32_t chunk, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 20554 +
 	       bit;	
-}
+}	//32 h2 indices
 
 static index_t indexH3(uint32_t chunk, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 20587 +
 	       bit;	
-}
+}	//32 h3 indices
 
 static index_t indexH4(uint32_t chunk, uint32_t bit) {
 	return INDICES_PER_CHUNK * chunk + 20620 +
 	       bit;	
-}
+}	//32 h4 indices
 
 static index_t indexGeneric(uint32_t chunk, uint32_t idx, uint32_t bit) { 
 	return INDICES_PER_CHUNK * chunk + 20653 + 
 	       idx * 32 + 
 	       bit;
-}
+}	//18080 generic indices
 
 int sha1sat(FILE * stream, size_t msize, const char * digest);
 
