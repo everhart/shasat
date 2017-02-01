a83657fad01d287ae4f08155c3d39e07d6152e43
everhart <rpe@rpe.io>
2017-02-01 12:13:17 -0500

    Added code for indexD() function.

--- sha1sat/sha1sat.c
+++ sha1sat/sha1sat.c
@@ -90,5 +90,11 @@ static index_t indexC(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	       bit;
 }
 
+static index_t indexD(uint32_t chunk, uint32_t idx, uint32_t bit) {
+	return INDICES_PER_CHUNK * chunk + 10244 +
+	       idx * 32 +
+	       bit;
+}
+
 int sha1sat(FILE * stream, size_t msize, const char * digest);
 
