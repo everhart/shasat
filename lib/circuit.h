cf210afdc0539482d8f4aa8530463a655634680e
everhart <rpe@rpe.io>
2017-02-01 10:40:44 -0500

    Revised prototypes and changed includes.

--- lib/circuit.h
+++ lib/circuit.h
@@ -1,61 +1,62 @@
 #ifndef CIRCUIT_H
 #define CIRCUIT_H
 
-#include "./routines.h"
+#include "./atom.h"
+#include "./bitops.h"
 
 int fwriteAtoms32(
 	FILE *		stream,
 	const uint32_t	inp[],
-	const uint32_t 	oup[],
-	uint64_t	leng
+	const index_t 	oup[],
+	size_t		leng
 );
 
 int fwriteAtoms64(
 	FILE *		stream,
-	const uint32_t	inp[],
-	const uint64_t 	oup[],
-	uint64_t	leng
+	const uint64_t	inp[],
+	const index_t 	oup[],
+	size_t		leng
 );
 
 int fwriteClauses(
 	FILE *		stream,
-	int * 		ante, 
+	atom_t * 	ante, 
 	size_t 		aleng, 
-	int * 		cons, 
+	atom_t * 	cons, 
 	size_t 		cleng
 );
 
 int fwriteAssignClauses(
 	FILE *		stream,
 	size_t		wsize,
-	uint32_t	inp,
-	uint32_t	oup	
+	index_t		inp,
+	index_t		oup	
 );
 
 int fwriteRshClauses(
 	FILE *		stream,
 	size_t		wsize,
-	uint32_t	inp,
-	uint32_t	oup,
+	index_t		inp,
+	index_t		oup,
 	uint32_t	rsh
 );
 
 int fwriteSumClauses(
 	FILE *		stream,
 	size_t		wsize,
-	uint32_t	inp1,
-	uint32_t	inp2,
-	uint32_t	sum,
-	uint32_t	crr
+	index_t		inp1,
+	index_t		inp2,
+	index_t		sum,
+	index_t		crr
 );
 
 int fwriteLroSumClauses(
 	FILE *		stream,
 	size_t		wsize,
-	uint32_t	inp1,
-	uint32_t	inp2,
-	uint32_t	sum,
-	uint32_t	crr,
+	index_t		inp1,
+	index_t		inp2,
+	index_t		sum,
+	index_t		crr,
 	uint32_t	lro
 );
 
