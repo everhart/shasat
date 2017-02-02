2b88ba67e100c98a40a60e7250cd2b0cfc9128ec
everhart <rpe@rpe.io>
2017-02-02 11:00:53 -0500

    Added code for fwriteEClauses() function.

--- sha1sat/sha1sat.c
+++ sha1sat/sha1sat.c
@@ -237,6 +237,21 @@ int fwriteFClauses(SHA1SAT sha1sat) {
 	return 0;
 }
 
+int fwriteEClauses(SHA1SAT * sha1sat) {
+	int res = 0;
+
+	sha1sat->e = indexE(sha1sat->chunk, sha1sat->loop, 0);
+	
+	res = fwriteAssignClauses(
+		sha1sat->stream, 32, sha1sat->d, sha1sat->e
+	);
+	if (res < 0) {
+		return -1;
+	}
+
+	return 0;
+}
+
 int sha1sat(FILE * stream, size_t msize, const char * digest) {
 	return 0;
 }
