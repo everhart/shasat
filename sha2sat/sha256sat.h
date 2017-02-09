75ad789ef5b01b67b81bcc2fdecfe0efc426be81
everhart <rpe@rpe.io>
2017-02-09 11:36:55 -0500

    Added sha256sat.h file.

--- sha2sat/sha256sat.h
+++ sha2sat/sha256sat.h
@@ -2,51 +2,16 @@
 #define SHA256SAT_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "../shasat.h"
+#include "../lib/atom.h"
+#include "../lib/bitops.h"
+#include "../lib/circuit.h"
 
-#define ATOMS_PER_CHUNK_SHA256 0
-
-uint32_t indexKBitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexWBitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexWvBitSha256(
-	uint8_t type, uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexHhBitSha256(
-	uint8_t type, uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexEp0BitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexEp1BitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexSig0BitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexSig1BitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexChBitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexMajBitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexTemp1BitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexTemp2BitSha256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
-uint32_t indexGenericBitSHA256(
-	uint32_t chunk, uint32_t word, uint32_t bit
-);
+int sha224sat(FILE * stream, size_t msize, const char * digest);
+int sha256sat(FILE * stream, size_t msize, const char * digest);
 
 #endif
