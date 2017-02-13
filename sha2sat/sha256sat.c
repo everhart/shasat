eea98c47748db6e444e21d79a8b8b3827c5a6d25
everhart <rpe@rpe.io>
2017-02-13 09:13:25 -0500

    Added fwriteSIG0Clauses() function.

--- sha2sat/sha256sat.c
+++ sha2sat/sha256sat.c
@@ -112,3 +112,51 @@ static index_t indexGeneric(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	       idx * 32 + 
 	       bit;
 }	//25440 generic indices
+
+static int fwriteSIG0Clauses(SHA256SAT * shs) {
+	int res = 0;
+	bool comb[3] = { 0 },
+	     eval = 0;
+
+	atom_t ante[3] = { 0 },
+	       cons = 0;
+
+	res = fwriteRshClauses(
+		shs->stream, 
+		32,
+		shs->w[shs->loop - 15],
+		shs->generic,
+		3
+	);
+	if (res < 0) {
+		return -1;
+	}
+
+	for (int i = 0; i < (1 << 3); i++) {
+		*comb = nextCombination(comb, 3);
+		eval = comb[0] ^ comb[1] ^ comb[2];
+
+		for (int j = 0; j < 32; j++) {
+			ante[0] = signAtom(
+				shs->w[shs->loop - 15] + bitPosRro(32, j, 7),
+				comb[0]
+			);
+			ante[1] = signAtom(
+				shs->w[shs->loop - 15] + bitPosRro(32, j, 18),
+				comb[1]
+			);
+
+			ante[2] = signAtom(shs->generic + j, comb[2]);
+			cons = signAtom(shs->SIG0 + j, eval);
+
+			res = fwriteClauses(
+				shs->stream, ante, 3, &cons, 1
+			);
+			if (res < 0) {
+				return -1;
+			}
+		}
+	}
+
+	return 0;
+}
