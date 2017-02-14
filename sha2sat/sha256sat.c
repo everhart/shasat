ee0d4b33bd44f087795577a04203ae1a27d3a0c9
everhart <rpe@rpe.io>
2017-02-14 08:03:09 -0500

    Changed fwriteSig1Clauses() function to use on fwriteSigClauses() in shacircuit.h.

--- sha2sat/sha256sat.c
+++ sha2sat/sha256sat.c
@@ -113,7 +113,7 @@ static index_t indexGeneric(uint32_t chunk, uint32_t idx, uint32_t bit) {
 	       bit;
 }	//25440 generic indices
 
-static int fwriteSig0Clauses(Sha256Sat * shs) {
+static int fwriteSig0ClausesSha256(Sha256Sat * shs) {
 	int res = fwriteSigClausesSha(
 		shs->stream,
 		32,
@@ -131,52 +131,22 @@ static int fwriteSig0Clauses(Sha256Sat * shs) {
 	return 0;
 }
 
-static int fwriteSIG1Clauses(SHA256SAT * shs) {
-	int res = 0;
-	bool comb[3] = { 0 },
-	     eval = 0;
-
-	atom_t ante[3] = { 0 },
-	       cons = 0;
-
-	const index_t w = shs->w[shs->loop - 2];
-
-	res = fwriteRshClauses(
-		shs->stream, 
+static int fwriteSig1ClausesSha256(Sha256Sat * shs) {
+	int res = fwriteSigClausesSha(
+		shs->stream,
 		32,
-		w,
+		shs->sig1,
 		shs->generic,
-		10
+		shs->w[shs->loop - 2],
+		17, 19, 10
 	);
 	if (res < 0) {
 		return -1;
 	}
 
-	for (int i = 0; i < (1 << 3); i++) {
-		*comb = nextCombination(comb, 3);
-		eval = comb[0] ^ comb[1] ^ comb[2];
+	shs->generic = res;
 
-		for (int j = 0; j < 32; j++) {
-			ante[0] = signAtom(
-				w + bitPosRro(32, j, 17), comb[0]
-			);
-			ante[1] = signAtom(
-				w + bitPosRro(32, j, 19), comb[1]
-			);
-			ante[2] = signAtom(shs->generic + j, comb[2]);
-			
-			cons = signAtom(shs->SIG1 + j, eval);
-
-			res = fwriteClauses(
-				shs->stream, ante, 3, &cons, 1
-			);
-			if (res < 0) {
-				return -1;
-			}
-		}
-	}
-
-	shs->generic += 32;
+	return 0;
 
 	return 0;
 }
