b7c75f534ceb3194da84cdca8b4c3e488d70603d
everhart <rpe@rpe.io>
2017-02-14 08:10:39 -0500

    Changed fwriteEp0Clauses() function to use on fwriteEpClausesSha() in shacircuit.h.

--- sha2sat/sha256sat.c
+++ sha2sat/sha256sat.c
@@ -171,44 +171,14 @@ static int fwriteWClausesSha256(Sha256Sat * shs) {
 	return 0;
 }
 
-static int fwriteEP0Clauses(SHA256SAT shs) {
-	int res = 0;
-	bool comb[3] = { 0 },
-	     eval = 0;
-
-	atom_t ante[3] = { 0 },
-	       cons = 0;
-
-	for (int i = 0; i < (1 << 3); i++) {
-		*comb = nextCombination(comb, 3);
-		eval = (comb[0] ^ comb[1] ^ comb[2]);
-
-		for (int j = 0; j < 32; j++) {
-			ante[0] = signAtom(
-				shs.cc[4] + bitPosRro(32, j, 6), 
-				comb[0]
-			);
-			ante[1] = signAtom(
-				shs.cc[4] + bitPosRro(32, j, 11), 
-				comb[1]
-			);
-			ante[2] = signAtom(
-				shs.cc[4] + bitPosRro(32, j, 25), 
-				comb[1]
-			);
-
-			cons = signAtom(shs.EP0 + j, eval);
-
-			res = fwriteClauses(
-				shs.stream, ante, 3, &cons, 1
-			);
-			if (res < 0) {
-				return -1;
-			}
-		}	
-	}
-
-	return 0;
+static int fwriteEp0ClausesSha256(Sha256Sat shs) {
+	return fwriteEpClausesSha(
+		shs.stream,
+		32,
+		shs.ep0,
+		shs.cc[4],
+		6, 11, 25
+	);
 }
 
 static int fwriteCHClauses(SHA256SAT shs) {
