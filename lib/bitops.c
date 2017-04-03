395fa92d454a656bcfd4867fec3e0deaf2519c90
everhart <rpe@rpe.io>
2017-04-03 07:31:42 -0400

    Renamed bitPos32() function to word32_bit() and updated code.

--- lib/bitops.c
+++ lib/bitops.c
@@ -1,11 +1,7 @@
 #include "./bitops.h"
 
-int bitVal32(uint32_t word, uint32_t pos) {
-	if (pos >= 32) {
-		return -1;
-	}
-
-	return (word >> pos) & 1;
+int word32_bit(uint32_t word, size_t n) {
+	return (n < 32) ? (word >> n) & 1 : -1;
 }
 
 int bitVal64(uint64_t word, uint32_t pos) {
