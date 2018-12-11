0567ab129825701b977384a2d72af98f89a1104f
everhart <rpe@rpe.io>
2018-12-11 07:27:27 -0500

    Fixed old, outdated includes.

--- sha2sat/sha256sat.h
+++ sha2sat/sha256sat.h
@@ -7,10 +7,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../lib/atom.h"
+#include "../lib/types.h"
 #include "../lib/bitops.h"
 #include "../lib/circuit.h"
-#include "../shacircuit.h"
 
 int sha224sat(FILE * stream, size_t msize, const char * digest);
 int sha256sat(FILE * stream, size_t msize, const char * digest);
